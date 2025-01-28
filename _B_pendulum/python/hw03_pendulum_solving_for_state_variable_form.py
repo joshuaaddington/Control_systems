6051951cc0486ec76b8d9528f2d39d00047b1ecb
marc-killpack <marc_killpack@byu.edu>
2025-01-28 13:39:49 -0700

    changes to allow us to save the Sympy-generated EOM and re-use them in pendulumDynamics.py

--- _B_pendulum/python/hw03_pendulum_solving_for_state_variable_form.py
+++ _B_pendulum/python/hw03_pendulum_solving_for_state_variable_form.py
@@ -6,7 +6,13 @@ from hw02_pendulum_finding_KE import *
 
 #%%
 #defining potential energy 
-P = m1*g*ell/2.0*(cos(theta)-1)
+P = m1*g*ell/2.0*(cos(theta)-1)    #this is "mgh", where "h" is a function of generalized coordinate "q"
+
+# can also do the following to get the same answer
+#   g_vec = Matrix([[0], [g], [0]])  # defining gravity in the direction that increases potential energy
+#   P = m1*g_vec.T@p1
+#   P = P[0,0]
+
 
 #calculate the lagrangian, using simplify intermittently can help the equations to be
 #simpler, there are also options for factoring and grouping if you look at the sympy
@@ -40,7 +46,11 @@ RHS = Matrix([[F - b*zd], [0]])
 full_eom = EL_case_studyB - RHS
 
 # finding and assigning zdd and thetadd
+# if our eom were more complicated, we could rearrange, solve for the mass matrix, and invert it to move it to the other side and find qdd and thetadd
 result = simplify(sp.solve(full_eom, (zdd, thetadd)))
+
+#TODO - add an example of finding the same thing, but not using sp.solve
+
 
 # result is a Python dictionary, we get to the entries we are interested in
 # by using the name of the variable that we were solving for
@@ -50,50 +60,61 @@ thetadd_eom = result[thetadd] # EOM for thetadd, as a function of states and inp
 display(Math(vlatex(zdd_eom)))
 display(Math(vlatex(thetadd_eom)))
 
-mass_matrix = Matrix([[m1+m2, m1*ell/2*cos(theta)], [m1*ell/2*cos(theta), m1*ell**2/3]])
 
 #%% [markdown]
-# Let's take a look at the inverse of the mass matrix (to compare against the solution shown in the book). 
-
-# %%
-display(Math(vlatex(mass_matrix.inv())))
-
-
-
-#%% [markdown]
-# OK, now we can see if I can get the state variable form of the equations of motion.
+# OK, now we can get the state variable form of the equations of motion.
 
 #%%
+import pendulumParam as P
+
 # defining fixed parameters that are not states or inputs (like g, ell, m1, m2, b)
-params = [(m1, 0.25), (m2, 1.), (ell, 1.), (g, 9.8), (b, 0.05)]
+# can be done like follows:
+# params = [(m1, P.m1), (m2, P.m2), (ell, P.ell), (g, P.g), (b, P.b)]  
+
+# but in this example, I want to keep the masses, length, and damping as variables so
+# that I can simulate uncertainty in those parameters in real life. 
+params = [(g, P.g)]
+
 
 # substituting parameters into the equations of motion
 zdd_eom = zdd_eom.subs(params)
 thetadd_eom = thetadd_eom.subs(params)
 
 # now defining the state variables that will be passed into f(x,u) 
-state = [zd, z, thetad, theta]
+state = [z, theta, zd, thetad]
 ctrl_input = [F]
 
 # defining the function that will be called to get the derivatives of the states
-state_dot = [zdd_eom, zd, thetadd_eom, thetad]
+state_dot = [zd, thetad, zdd_eom, thetadd_eom]
+
+
+#%%
+import numpy as np
 
 # converting the function to a callable function that uses numpy to evaluate and 
 # return a list of state derivatives
-f = sp.lambdify(state + ctrl_input, state_dot)
+eom = sp.lambdify([state, ctrl_input, m1, m2, ell, b], np.array(state_dot), 'numpy')
 
 # calling the function as a test to see if it works:
+cur_state = [0, 0, 0, 0]
+cur_input = [1]
+print("x_dot = ", eom(cur_state, cur_input, P.m1, P.m2, P.ell, P.b))
 
-# f(zd, z, thetad, theta, F)
-print("x_dot = ", f(0, 0, 0, 0, 1))
+
+
 
 #%% [markdown] 
-# The next step would be to call this function "f" from a numerical integrator, like scipy.integrate.ivp.solve_ivp 
-# or rk4 functions in the case studies. 
+# The next step is to save this function "f" so that we can use it with a numerical integrator, like 
+# scipy.integrate.ivp.solve_ivp or the rk4 functions in the case studies. To save this function, we do the
+# following:
 
-# Although not covered in this class, there are ways to store this function and just re-load it
-# when you want to use it, so that you don't have to re-run the sympy code every time you want to
-# perform a simulation. See https://stackoverflow.com/questions/29079923/save-load-sympy-lambdifed-expressions
+#%%
+import dill   # we may have to install dill using pip "python -m pip install dill"
+dill.settings['recurse'] = True
+dill.dump(eom, open("eom_case_study_B", "wb"))   #takes the function name "eom" and writes it to a file called "eom_case_study_B"
 
 
-# %%
+# we can then reload the function and test it again to make sure it works: 
+eom_test = dill.load(open("eom_case_study_B", "rb"))
+
+print("x_dot after reloading = ", eom(cur_state, cur_input, P.m1, P.m2, P.ell, P.b))
